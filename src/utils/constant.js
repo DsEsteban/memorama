2cdb4b5d30630c8b4980e67cfdcf4859d89bdbce
dsesteban <dennis.esteban.89@gmail.com>
2021-03-22 16:47:02 -0400

    random cards order

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,27 @@
+const CARDS_SYMBOLS = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10];
+
+const shuffle = (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+}
+
+const COPY_CARDS_SYMBOLS = shuffle(CARDS_SYMBOLS);
+
 export const cards = new Map();
-for (let i = 1; i<21;i++) {
-    cards.set(i, { id:i, symbol: i % 2 === 0 ? i -1 : i , isFaceUp:false, belongsTo: null })
+for (let i = 0; i < COPY_CARDS_SYMBOLS.length; i++) {
+    cards.set(i, { id:i, symbol: COPY_CARDS_SYMBOLS[i], isFaceUp:false, belongsTo: null })
 }
