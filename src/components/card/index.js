68cfbe905a789c8611ff98640fd1e59bacee6e70
dsesteban <dennis.esteban.89@gmail.com>
2021-03-21 23:23:01 -0400

    card component UI

---
+++ src/components/card/index.js
@@ -0,0 +1,13 @@
+import './card.css';
+
+const Card = () => {
+  return (
+    <div className="card-container">
+        <div className="card-content">
+            1
+        </div>
+    </div>
+  );
+}
+
+export default Card;
